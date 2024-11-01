2d74229d55f4800aa827a3647c636e28459824b1
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-11-01 21:04:48 +0530

    Home.js

---
+++ ebookie/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="p-4">
+            <h1 className="text-3xl font-bold">Welcome to E-Bookie!</h1>
+            <p>Your gateway to a world of stories, knowledge, and imagination.</p>
+        </div>
+    );
+};
+
+export default Home;
